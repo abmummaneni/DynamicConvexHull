5a8ece1415f44edb78ed6d110a612e959da1a03d
Abhi Mummaneni <abhinav.mummaneni@tufts.edu>
2023-10-29 21:25:34 -0400

    More testing

--- main.cpp
+++ main.cpp
@@ -25,5 +25,22 @@ int main() {
         t.displayTree();
         t.checkProperties();
     }
+    points.clear();
+    for (int i = 0; i < 40; ++i) {
+        Point randPoint = Point(random() % 100, random() % 100);
+        points.push_back(randPoint);
+        cout << "inserting " << i << " " << randPoint.x << endl;
+        t.insert(randPoint);
+        t.displayTree();
+        cout << "checking properties" << endl;
+        t.checkProperties();
+    }
+    std::shuffle(points.begin(), points.end(), std::mt19937(std::random_device()()));
+    for (Point p : points) {
+        cout << "removing " << p.x << endl;
+        t.remove(p);
+        t.displayTree();
+        t.checkProperties();
+    }
     return 0;
 }
